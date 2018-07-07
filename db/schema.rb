23dd01da564984dd334bbe0aaf9d16230eddc6f0
grenn10 <grenn10@naver.com>
2018-07-07 23:21:58 +0900

    정원

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,13 @@ ActiveRecord::Schema.define(version: 20180707043327) do
     t.integer "user_id"
     t.text "bookname"
     t.text "author"
+    t.string "classname"
     t.integer "price"
+    t.string "major"
+    t.integer "state"
     t.string "img_url"
+    t.integer "method"
     t.text "content"
-    t.boolean "sell", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
@@ -36,6 +39,16 @@ ActiveRecord::Schema.define(version: 20180707043327) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
@@ -43,6 +56,16 @@ ActiveRecord::Schema.define(version: 20180707043327) do
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_likes_on_book_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "new_notifications", force: :cascade do |t|
@@ -78,8 +101,6 @@ ActiveRecord::Schema.define(version: 20180707043327) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "username", default: "", null: false
-    t.string "school", default: "", null: false
-    t.integer "studentid", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -89,6 +110,13 @@ ActiveRecord::Schema.define(version: 20180707043327) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
