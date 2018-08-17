69a939d3775082ed968a2a5548175c7f6c662f5e
gmlwls <gmlwlssg13@naver.com>
2018-08-17 21:10:01 +0900

    대댓글 알림

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -12,6 +12,10 @@ class RepliesController < ApplicationController
     @reply.user_id = current_user.id
     @reply.save
 
+    if @book.user_id != current_user.id
+      @new_notification = NewNotification.create! user: @book.user, content: "#{current_user.username}님이 #{@book.bookname}책에 대댓글을 달았습니다." ,content_truncate: "#{current_user.username}님이 #{@book.bookname}책에 대댓글을 달았습니다.".truncate(20, omission: '...'), link: book_path(@book)
+    end
+
     redirect_to "/books/#{@book.id}/#reply-#{@reply.id}"
 
   end
